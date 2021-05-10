8f83db528d7dc0305a8cfcffd1db7988fcec8f13
et77 <81599370+pon56@users.noreply.github.com>
2021-05-11 07:02:39 +0900

    アップロード

---
+++ script.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 2.9,
+    spaceBetween: 40,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+
+//   Q&Aトグル
+$(".qa-box-q").on('click', function () {
+    $(this).next().slideToggle()
+    $(this).children('.qa-box-icon').toggleClass("is-open")
+})
