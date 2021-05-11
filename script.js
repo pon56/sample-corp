4c95d73d6400461e516a0c0b772f114e482f01b8
et77 <81599370+pon56@users.noreply.github.com>
2021-05-12 00:03:37 +0900

    スマホ版　topまで

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ $(".qa-box-q").on('click', function () {
     $(this).next().slideToggle()
     $(this).children('.qa-box-icon').toggleClass("is-open")
 })
+
+// トップへの処理
+$(window).on('scroll', function(){
+    if (100 < $(this).scrollTop()){
+      $(".to-top").addClass("is-active")
+    }else{
+      $(".to-top").removeClass("is-active")
+    }
+  })
+  
+  $('a[href^="#"]').on('click', function(){
+  
+    var id= $(this).attr('href')
+    var headerSize = $("header").innerHeight()
+    var position = 0
+  
+    if( id != "#"){
+      position = $(id).offset().top-headerSize
+    }
+    
+    $('html,body').animate({
+      scrollTop: position
+    },400)
+  })
